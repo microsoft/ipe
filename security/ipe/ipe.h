4f66ccc81681126ad11d21fbfe934e60b0044884
Deven Bowers <deven.desai@linux.microsoft.com>
2022-12-12 15:08:28 -0800

    security: add ipe lsm
    Integrity Policy Enforcement (IPE) is an LSM that provides an
    complimentary approach to Mandatory Access Control than existing LSMs
    today.
    
    Existing LSMs have centered around the concept of access to a resource
    should be controlled by the current user's credentials. IPE's approach,
    is that access to a resource should be controlled by the system's trust
    of a current resource.
    
    The basis of this approach is defining a global policy to specify which
    resource can be trusted.
    
    Signed-off-by: Deven Bowers <deven.desai@linux.microsoft.com>
    Signed-off-by: Fan Wu <wufan@linux.microsoft.com>
    ---
    v2:
      + Split evaluation loop, access control hooks,
        and evaluation loop from policy parser and userspace
        interface to pass mailing list character limit
    
    v3:
      + Move ipe_load_properties to patch 04.
      + Remove useless 0-initializations
      + Prefix extern variables with ipe_
      + Remove kernel module parameters, as these are
        exposed through sysctls.
      + Add more prose to the IPE base config option
        help text.
      + Use GFP_KERNEL for audit_log_start.
      + Remove unnecessary caching system.
      + Remove comments from headers
      + Use rcu_access_pointer for rcu-pointer null check
      + Remove usage of reqprot; use prot only.
      + Move policy load and activation audit event to 03/12
    
    v4:
      + Remove sysctls in favor of securityfs nodes
      + Re-add kernel module parameters, as these are now
        exposed through securityfs.
      + Refactor property audit loop to a separate function.
    
    v5:
      + fix minor grammatical errors
      + do not group rule by curly-brace in audit record,
        reconstruct the exact rule.
    
    v6:
      + No changes
    
    v7:
      + Further split lsm creation into a separate commit from the
        evaluation loop and audit system, for easier review.
    
      + Introduce the concept of an ipe_context, a scoped way to
        introduce execution policies, used initially for allowing for
        kunit tests in isolation.
    
    v8:
      + Follow lsmname_hook_name convention for lsm hooks.
      + Move LSM blob accessors to ipe.c and mark LSM blobs as static.
    
    v9:
      + Remove ipe_context for simplification
    
    v10:
      + Add github url
    
    v11:
      + Correct github url
      + Move ipe before bpf
    
    v12:
      + Switch to use lsm_id instead of string for lsm name
    
    v13:
      + No changes
    
    v14:
      + No changes
    
    v15:
      + Add missing code in tools/testing/selftests/lsm/lsm_list_modules_test.c
    
    v16:
      + No changes

---
+++ security/ipe/ipe.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ */
+
+#ifndef _IPE_H
+#define _IPE_H
+
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) "IPE: " fmt
+
+#include <linux/lsm_hooks.h>
+
+#endif /* _IPE_H */
