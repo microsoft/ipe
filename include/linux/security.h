7053459e0c62b6fdbd70fc720a5d06b71ceb8ef1
Deven Bowers <deven.desai@linux.microsoft.com>
2022-12-12 15:09:17 -0800

    block|security: add LSM blob to block_device
    Some block devices have valuable security properties that is only
    accessible during the creation time.
    
    For example, when creating a dm-verity block device, the dm-verity's
    roothash and roothash signature, which are extreme important security
    metadata, are passed to the kernel. However, the roothash will be saved
    privately in dm-verity, which prevents the seucrity subsystem to easily
    access that information. Worse, in the current implementation the
    roothash signature will be discarded after the verification, making it
    impossible to utilize the roothash signature by the security subsystem.
    
    With this patch, an LSM blob is added to the block_device structure.
    This enables the security subsystem to store security-sensitive data
    related to block devices within the security blob. For example, LSM can
    use the new LSM blob to save the roothash signature of a dm-verity,
    and LSM can make access decision based on the data inside the signature,
    like the signer ceritificate.
    
    The implementation follows the same approach used for security blobs in
    other structures like struct file, struct inode, and struct superblock.
    The initialization of the security blob occurs after the creation of the
    struct block_device, performed by the security subsystem. Similarly, the
    security blob is freed by the security subsystem before the struct
    block_device is deallocated or freed.
    
    Signed-off-by: Deven Bowers <deven.desai@linux.microsoft.com>
    Signed-off-by: Fan Wu <wufan@linux.microsoft.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    
    ---
    v2:
      + No Changes
    
    v3:
      + Minor style changes from checkpatch --strict
    
    v4:
      + No Changes
    
    v5:
      + Allow multiple callers to call security_bdev_setsecurity
    
    v6:
      + Simplify security_bdev_setsecurity break condition
    
    v7:
      + Squash all dm-verity related patches to two patches,
        the additions to dm-verity/fs, and the consumption of
        the additions.
    
    v8:
      + Split dm-verity related patches squashed in v7 to 3 commits based on
        topic:
          + New LSM hook
          + Consumption of hook outside LSM
          + Consumption of hook inside LSM.
    
      + change return of security_bdev_alloc / security_bdev_setsecurity
        to LSM_RET_DEFAULT instead of 0.
    
      + Change return code to -EOPNOTSUPP, bring inline with other
        setsecurity hooks.
    
    v9:
      + Add Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
      + Remove unlikely when calling LSM hook
      + Make the security field dependent on CONFIG_SECURITY
    
    v10:
      + No changes
    
    v11:
      + No changes
    
    v12:
      + No changes

--- include/linux/security.h
+++ include/linux/security.h
@@ -496,6 +496,11 @@ int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen);
 int security_locked_down(enum lockdown_reason what);
 int lsm_fill_user_ctx(struct lsm_ctx __user *uctx, size_t *uctx_len,
 		      void *val, size_t val_len, u64 id, u64 flags);
+int security_bdev_alloc(struct block_device *bdev);
+void security_bdev_free(struct block_device *bdev);
+int security_bdev_setsecurity(struct block_device *bdev,
+			      const char *name, const void *value,
+			      size_t size);
 #else /* CONFIG_SECURITY */
 
 static inline int call_blocking_lsm_notifier(enum lsm_event event, void *data)
@@ -1439,6 +1444,23 @@ static inline int lsm_fill_user_ctx(struct lsm_ctx __user *uctx,
 {
 	return -EOPNOTSUPP;
 }
+
+static inline int security_bdev_alloc(struct block_device *bdev)
+{
+	return 0;
+}
+
+static inline void security_bdev_free(struct block_device *bdev)
+{
+}
+
+static inline int security_bdev_setsecurity(struct block_device *bdev,
+					    const char *name,
+					    const void *value, size_t size)
+{
+	return 0;
+}
+
 #endif	/* CONFIG_SECURITY */
 
 #if defined(CONFIG_SECURITY) && defined(CONFIG_WATCH_QUEUE)
